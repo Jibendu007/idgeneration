c9e011cbbf903dbb3c71282569bd2b2ffb918e1b
jibendu.paul <jibendu.paul@volantetech.com>
2024-05-28 10:24:18 +0530

    Test cases updated

--- src/test/java/com/volante/idgeneration/IdgenerationApplicationTests.java
+++ src/test/java/com/volante/idgeneration/IdgenerationApplicationTests.java
@@ -1,13 +1,51 @@
+//package com.volante.idgeneration;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class IdgenerationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
 package com.volante.idgeneration;
 
+import com.volante.idgeneration.service.IdGenerationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class IdgenerationApplicationTests {
+
+	@Autowired
+	private IdGenerationService idGenerationService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void generatedIdsShouldBeUnique() throws IOException {
+		Set<String> ids = new HashSet<>();
+		int numIds = 10; // Number of IDs to generate and check
+		String payloadJson = "{ \"type\": \"interface\", \"name\": \"Credittransfertrace\" }";
+		for (int i = 0; i < numIds; i++) {
+			String id = idGenerationService.generateId(payloadJson);
+			ids.add(id);
+		}
+
+		// Assert that the number of unique IDs generated matches the number of IDs requested
+		assertEquals(numIds, ids.size());
+	}
 }
+
